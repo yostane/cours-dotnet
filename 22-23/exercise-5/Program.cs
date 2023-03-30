f5a076a2d23f2f19511ae0f3e06b52c2fb912d97
yostane <1958676+yostane@users.noreply.github.com>
2023-03-30 16:59:30 +0200

    Correction exo 5

--- 22-23/exercise-5/Program.cs
+++ 22-23/exercise-5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 Circle c1 = new(10);
 Circle c2 = new(10);
@@ -27,3 +29,102 @@ void PrintShapeInfo(Shape shape)
 
 PrintShapeInfo(c1);
 PrintShapeInfo(new Rectangle(5, 20));
+
+List<Shape> GenerateRandomShapes(int count)
+{
+    Random r = new();
+    List<Shape> shapes = new();
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine("Hello");
+        if (r.Next(2) == 1)
+        {
+            shapes.Add(new Circle(r.Next(2, 20)));
+        }
+        else
+        {
+            shapes.Add(new Rectangle(r.Next(2, 20), r.Next(2, 20)));
+        }
+    }
+    return shapes;
+}
+
+var shapes = GenerateRandomShapes(10);
+Console.WriteLine(string.Join("\n", shapes));
+
+// yield génère les éléments à la demande (et pas tout d'une coup)
+IEnumerable<Shape> GenerateRandomShapes2(int count)
+{
+    Random r = new();
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine("Hello yield");
+        if (r.Next(2) == 1)
+        {
+            yield return new Circle(r.Next(2, 20));
+        }
+        else
+        {
+            yield return new Rectangle(r.Next(2, 20), r.Next(2, 20));
+        }
+    }
+}
+var yieldedShapes = GenerateRandomShapes2(10);
+Console.WriteLine(string.Join("\n", yieldedShapes));
+
+foreach (var shape in yieldedShapes)
+{
+    Console.WriteLine(shape);
+}
+
+// where, select, aggregate
+IEnumerable<Shape> GenerateRandomShapesLinq(int count)
+{
+    Random r = new();
+    return Enumerable.Range(0, count + 1).Select((_) =>
+    {
+        if (r.Next(2) == 1)
+        {
+            return new Circle(r.Next(2, 20)) as Shape;
+        }
+        else
+        {
+            return new Rectangle(r.Next(2, 20), r.Next(2, 20)) as Shape;
+        }
+    });
+}
+
+var shapesLinq = GenerateRandomShapesLinq(10);
+Console.WriteLine(string.Join("\n", shapesLinq));
+
+var q8_1 = new
+{
+    maxWidth = shapes.Where((shape) => shape is Rectangle)
+                                .Select((shape) => shape as Rectangle)
+                                .Max((rectangle) => rectangle?.Width),
+    sumCircleArea = shapes.Where((shape) => shape is Circle)
+                            .Select((shape) => shape as Circle)
+                            .Sum((circle) => circle?.Area)
+};
+Console.WriteLine(q8_1);
+
+// var q8_2 = shapes.Select((shape) =>
+// {
+//     if (shape is Circle c)
+//     {
+//         return new { maxWidth = 0, sumCircleArea = c.Area };
+//     }
+//     else if (shape is Rectangle r)
+//     {
+//         return new { maxWidth = r.Width, sumCircleArea = 0 };
+//     }
+// });
+
+var q9 = shapes.Where((shape) => shape is Circle).Count();
+Console.WriteLine(q9);
+
+var q10 = shapes.Where((shape) => shape is Rectangle && shape.Area > 10).Count();
+Console.WriteLine(q10);
+
+var q11 = shapes.Where((shape) => shape is Circle).Average((s) => s.Circumference);
+Console.WriteLine(q11);
